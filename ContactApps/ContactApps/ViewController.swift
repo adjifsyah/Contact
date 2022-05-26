d57904aacd0eb20ff09abb938a4d07aba6550961
Adji Firmansyah <adji.firmansyah27@icloud.com>
2022-05-26 21:39:45 +0700

    Add Project

---
+++ ContactApps/ContactApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactApps
+//
+//  Created by Adji Firmansyah on 5/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
